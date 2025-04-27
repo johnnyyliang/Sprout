148ae296f2af1652295d07deb25cfe100d08757b
Arnav Kaul <68097515+kaul-arnav@users.noreply.github.com>
2025-04-27 15:21:23 -0400

    modify tab title and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Eco-Invest",
+  title: "Sprout",
   description: "Analyze companies based on their environmental impact and growth potential",
 };
 
